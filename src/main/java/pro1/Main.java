26781389fb3265804fa541369462b752fa232ce0
matoulu2 <matoulu2@uhk.cz>
2025-02-10 09:47:47 +0100

    Zkoumam, kolik je 0.1+0.2

--- src/main/java/pro1/Main.java
+++ src/main/java/pro1/Main.java
@@ -4,5 +4,9 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello, World!");
+        double x = 0.3;
+        double y = 0.2;
+        double z = 0.1;
+        System.out.println(0.3 - (0.1 + 0.2));
     }
 }
